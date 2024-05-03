9743432bbd4e3eec97ebfe46282a72117f4d1b2e
KASHIF-SHOUKAT-SE <149269224+KASHIF-SHOUKAT-SE@users.noreply.github.com>
2024-05-03 11:39:11 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+document.write("hello world");
+///////settimeout////
+// const c = (a,b)=>{
+//     console.log("yes i am runing" + (a+b))
+//     a+b
+// }
+// setTimeout(c,1000,1,2)
+// let a = setTimeout(function(){
+//     alert("after 2 second show form my screen")
+// }, 2000);
+// let b = prompt("do you want to run settimeout");
+// if("n" == b){
+//     clearTimeout(a)
+// }
+// // clearTimeout(a)
+// console.log(a)
+
+/////setinterval////
+// setInterval(function(){
+//     alert("this is setinterval")
+// },3000)
+
